0a07dfb6816f3e4381cda12780a651346b0aaab9
Akatsuki Rui <3736910+akiirui@users.noreply.github.com>
2024-05-12 08:02:34 +0800

    release: 2024.05.12

--- userscript.js
+++ userscript.js
@@ -8,7 +8,7 @@
 // @description:zh-CN   通过 mpv-handler 播放网页上的视频和歌曲
 // @description:zh-TW   通過 mpv-handler 播放網頁上的視頻和歌曲
 // @namespace           play-with-mpv-handler
-// @version             2024.04.22.3
+// @version             2024.05.12
 // @author              Akatsuki Rui
 // @license             MIT License
 // @require             https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@06f2015c04db3aaab9717298394ca4f025802873/gm_config.js
