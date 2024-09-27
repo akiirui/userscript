f4139b321e3264e034b65240b4aa6561a47ebd67
Akatsuki Rui <3736910+akiirui@users.noreply.github.com>
2024-09-28 00:10:05 +0800

    fix: firefox blank window

--- userscript.js
+++ userscript.js
@@ -8,7 +8,7 @@
 // @description:zh-CN   通过 mpv-handler 播放网页上的视频和歌曲 (NSFW)
 // @description:zh-TW   通過 mpv-handler 播放網頁上的視頻和歌曲 (NSFW)
 // @namespace           play-with-mpv-handler-nsfw
-// @version             2024.08.09
+// @version             2024.09.27
 // @author              Akatsuki Rui
 // @license             MIT License
 // @require             https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@06f2015c04db3aaab9717298394ca4f025802873/gm_config.js
@@ -351,7 +351,6 @@ function createButton() {
   if (body) {
     buttonPlay.className = "pwm-play";
     buttonPlay.style = "display: none";
-    buttonPlay.target = "_blank";
     buttonPlay.addEventListener("click", pauseVideo);
 
     buttonSettings.className = "pwm-settings";
