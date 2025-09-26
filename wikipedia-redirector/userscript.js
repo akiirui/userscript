2c758dd515d22ec0aa2c0a1d16099e602443d94d
Akatsuki Rui <3736910+akiirui@users.noreply.github.com>
2025-09-26 13:37:58 +0800

    fix(wikipedia-redirector): add missing /zh/ url

--- wikipedia-redirector/userscript.js
+++ wikipedia-redirector/userscript.js
@@ -6,17 +6,19 @@
 // @description:en-US   Automatically redirect Wikipedia language variants
 // @description:zh-CN   自动重定向维基百科语言变体
 // @namespace           wikipedia-redirector
-// @version             2025.09.06
+// @version             2025.09.26
 // @author              Akatsuki Rui
 // @license             MIT License
 // @run-at              document-start
 // @match               *://zh.wikipedia.org/wiki/*
+// @match               *://zh.wikipedia.org/zh/*
 // @match               *://zh.wikipedia.org/zh-hk/*
 // @match               *://zh.wikipedia.org/zh-mo/*
 // @match               *://zh.wikipedia.org/zh-my/*
 // @match               *://zh.wikipedia.org/zh-sg/*
 // @match               *://zh.wikipedia.org/zh-tw/*
 // @match               *://zh.m.wikipedia.org/wiki/*
+// @match               *://zh.m.wikipedia.org/zh/*
 // @match               *://zh.m.wikipedia.org/zh-hk/*
 // @match               *://zh.m.wikipedia.org/zh-mo/*
 // @match               *://zh.m.wikipedia.org/zh-my/*
